faa7e01f8149809632b712aaec5be9b7374c4bb1
kldio <nikos.klido@gmail.com>
2020-01-06 22:23:40 +0200

    stop propagation when opening context menu

--- js/components/playlist/sidebar-list.vue
+++ js/components/playlist/sidebar-list.vue
@@ -69,6 +69,7 @@ export default {
         if (this.creating) {
           this.creating = false
         } else {
+          event.stopImmediatePropagation();
           this.$refs.contextMenu.open(event.pageY, event.pageX)
         }
       })
